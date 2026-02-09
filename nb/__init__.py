04a5e043ee23c7cbb78e0266a4d6db569babc899
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-09 23:42:09 +0800

    Delete nb/__init__.py

--- nb/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Package nb.
-
-The ultimate tool to automate custom telegram message forwarding.
-https://github.com/artai8/nb
-"""
-
-from importlib.metadata import version
-
-__version__ = version(__package__)
