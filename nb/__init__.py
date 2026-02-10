c58172735bd0b99f17b8e61cf6f1a302c8dccdce
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-10 18:31:43 +0800

    Update __init__.py

--- nb/__init__.py
+++ nb/__init__.py
@@ -8,5 +8,4 @@ try:
     from importlib.metadata import version
     __version__ = version(__package__)
 except Exception:
-    # 如果包没有安装，使用硬编码版本
-    __version__ = "1.1.8"  # 从 pyproject.toml 中的版本号
+    __version__ = "1.1.8"
