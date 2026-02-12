e014e4579b806df52c185af76b13bbc965271d2d
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-13 00:01:50 +0800

    Update 2_⭐_Admins.py

--- nb/web_ui/pages/2_⭐_Admins.py
+++ nb/web_ui/pages/2_⭐_Admins.py
@@ -1,21 +1,15 @@
 import streamlit as st
-
 from nb.config import CONFIG, read_config, write_config
 from nb.web_ui.password import check_password
 from nb.web_ui.utils import get_list, get_string, hide_st, switch_theme
 
 CONFIG = read_config()
-
-st.set_page_config(
-    page_title="Admins",
-    page_icon="⭐",
-)
+st.set_page_config(page_title="管理员", page_icon="⭐")
 hide_st(st)
-switch_theme(st,CONFIG)
+switch_theme(st, CONFIG)
+
 if check_password(st):
-
-    CONFIG.admins = get_list(st.text_area("Admins", value=get_string(CONFIG.admins)))
-    st.write("Add the usernames of admins. One in each line.")
-
-    if st.button("Save"):
+    CONFIG.admins = get_list(st.text_area("管理员列表", value=get_string(CONFIG.admins)))
+    st.write("每行一个管理员用户名或 ID。")
+    if st.button("保存"):
         write_config(CONFIG)
