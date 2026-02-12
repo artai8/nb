1a7bfe95dcf7370a2ff7e55e0799b6324a688913
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-13 00:00:22 +0800

    Update password.py

--- nb/web_ui/password.py
+++ nb/web_ui/password.py
@@ -2,29 +2,18 @@ from nb.config import PASSWORD
 
 
 def check_password(st):
-    """Returns `True` if the user had the correct password."""
-
     def password_entered():
-        """Checks whether a password entered by the user is correct."""
         if st.session_state["password"] == PASSWORD:
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
+            del st.session_state["password"]
         else:
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
+        st.text_input("密码", type="password", on_change=password_entered, key="password")
         return False
     elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
+        st.text_input("密码", type="password", on_change=password_entered, key="password")
+        st.error("😕 密码错误")
         return False
-    else:
-        # Password correct.
-        return True
+    return True
