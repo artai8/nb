1d125e85dbf10cb00b720c5f9ed6d0ce3de0c974
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-14 20:49:12 +0800

    Add files via upload

--- nb/bot/live_bot.py
+++ nb/bot/live_bot.py
@@ -1,85 +1,145 @@
+"""A bot to controll settings for nb live mode."""
+
 import logging
+
 import yaml
 from telethon import events
-from nb import config
-from nb.bot.utils import admin_protect, display_forwards, get_args, get_command_prefix, remove_source
+
+from nb import config, const, plugins
+from nb.bot.utils import (
+    admin_protect,
+    display_forwards,
+    get_args,
+    get_command_prefix,
+    remove_source,
+)
 from nb.config import CONFIG, write_config
 from nb.plugin_models import Style
 
 
 @admin_protect
 async def forward_command_handler(event):
-    notes = "用法: /forward source: a\ndest: [b,c]"
+    """Handle the `/forward` command."""
+    notes = """The `/forward` command allows you to add a new forward.
+    Example: suppose you want to forward from a to (b and c)
+
+    ```
+    /forward source: a
+    dest: [b,c]
+    ```
+
+    a,b,c are chat ids
+
+    """.replace(
+        "    ", ""
+    )
+
     try:
         args = get_args(event.message.text)
         if not args:
-            raise ValueError(f"{notes}\n{display_forwards(CONFIG.forwards)}")
-        parsed = yaml.safe_load(args)
-        forward = config.Forward(**parsed)
+            raise ValueError(f"{notes}\n{display_forwards(config.CONFIG.forwards)}")
+
+        parsed_args = yaml.safe_load(args)
+        forward = config.Forward(**parsed_args)
         try:
-            remove_source(forward.source, CONFIG.forwards)
-        except Exception:
+            remove_source(forward.source, config.CONFIG.forwards)
+        except:
             pass
         CONFIG.forwards.append(forward)
-        config.from_to = await config.load_from_to(event.client, CONFIG.forwards)
-        await event.respond("成功")
-        write_config(CONFIG)
+        config.from_to = await config.load_from_to(event.client, config.CONFIG.forwards)
+
+        await event.respond("Success")
+        write_config(config.CONFIG)
     except ValueError as err:
+        logging.error(err)
         await event.respond(str(err))
+
     finally:
         raise events.StopPropagation
 
 
 @admin_protect
 async def remove_command_handler(event):
-    notes = "用法: /remove source: -100"
+    """Handle the /remove command."""
+    notes = """The `/remove` command allows you to remove a source from forwarding.
+    Example: Suppose you want to remove the channel with id -100, then run
+
+    `/remove source: -100`
+
+    """.replace(
+        "    ", ""
+    )
+
     try:
         args = get_args(event.message.text)
         if not args:
-            raise ValueError(f"{notes}\n{display_forwards(CONFIG.forwards)}")
-        parsed = yaml.safe_load(args)
-        CONFIG.forwards = remove_source(parsed.get("source"), CONFIG.forwards)
-        config.from_to = await config.load_from_to(event.client, CONFIG.forwards)
-        await event.respond("成功")
-        write_config(CONFIG)
+            raise ValueError(f"{notes}\n{display_forwards(config.CONFIG.forwards)}")
+
+        parsed_args = yaml.safe_load(args)
+        source_to_remove = parsed_args.get("source")
+        CONFIG.forwards = remove_source(source_to_remove, config.CONFIG.forwards)
+        config.from_to = await config.load_from_to(event.client, config.CONFIG.forwards)
+
+        await event.respond("Success")
+        write_config(config.CONFIG)
     except ValueError as err:
+        logging.error(err)
         await event.respond(str(err))
+
     finally:
         raise events.StopPropagation
 
 
 @admin_protect
 async def style_command_handler(event):
+    """Handle the /style command"""
+    notes = """This command is used to set the style of the messages to be forwarded.
+
+    Example: `/style bold`
+
+    Options are preserve,normal,bold,italics,code, strike
+
+    """.replace(
+        "    ", ""
+    )
+
     try:
         args = get_args(event.message.text)
         if not args:
-            raise ValueError("用法: /style bold\n选项: preserve,plain,bold,italics,code,strike")
-        valid = [i.value for i in Style]
-        if args not in valid:
-            raise ValueError(f"无效样式，可选: {valid}")
+            raise ValueError(f"{notes}\n")
+        _valid = [item.value for item in Style]
+        if args not in _valid:
+            raise ValueError(f"Invalid style. Choose from {_valid}")
         CONFIG.plugins.fmt.style = args
-        await event.respond("成功")
+        await event.respond("Success")
         write_config(CONFIG)
     except ValueError as err:
+        logging.error(err)
         await event.respond(str(err))
+
     finally:
         raise events.StopPropagation
 
 
 async def start_command_handler(event):
+    """Handle the /start command."""
     await event.respond(CONFIG.bot_messages.start)
 
 
 async def help_command_handler(event):
+    """Handle the /help command."""
     await event.respond(CONFIG.bot_messages.bot_help)
 
 
 def get_events():
     _ = get_command_prefix()
-    return {
+    logging.info(f"Command prefix is . for userbot and / for bot")
+    command_events = {
         "start": (start_command_handler, events.NewMessage(pattern=f"{_}start")),
         "forward": (forward_command_handler, events.NewMessage(pattern=f"{_}forward")),
         "remove": (remove_command_handler, events.NewMessage(pattern=f"{_}remove")),
         "style": (style_command_handler, events.NewMessage(pattern=f"{_}style")),
         "help": (help_command_handler, events.NewMessage(pattern=f"{_}help")),
     }
+
+    return command_events
