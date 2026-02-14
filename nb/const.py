6d9f3bfd03b94e56c4a97c0961a05d8196005117
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-14 15:47:42 +0800

    Delete nb/const.py

--- nb/const.py
+++
@@ -1,5 +0,0 @@
-COMMANDS = {"start": "检查是否在线", "forward": "设置新转发", "remove": "移除转发", "help": "使用帮助"}
-REGISTER_COMMANDS = True
-KEEP_LAST_MANY = 10000
-CONFIG_FILE_NAME = "nb.config.json"
-CONFIG_ENV_VAR_NAME = "NB_CONFIG"
