46094c0fad36dccfcd66896f7b98beaf23f13c99
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-14 19:54:18 +0800

    Add files via upload

--- nb/bot/utils.py
+++ nb/bot/utils.py
@@ -1,66 +1,44 @@
-"""helper functions for the bot."""
-import logging
-from typing import List
-
 from telethon import events
-
 from nb import config
 from nb.config import Forward
 
 
 def admin_protect(org_func):
-    """Decorate to restrict non admins from accessing the bot."""
-
     async def wrapper_func(event):
-        """Wrap the original function."""
-        logging.info(f"Applying admin protection! Admins are {config.ADMINS}")
         if event.sender_id not in config.ADMINS:
-            await event.respond("You are not authorized.")
+            await event.respond("无权操作")
             raise events.StopPropagation
         return await org_func(event)
-
     return wrapper_func
 
 
-def get_args(text: str) -> str:
-    """Return the part of message following the command."""
+def get_args(text):
     splitted = text.split(" ", 1)
-
-    if not len(splitted) == 2:
+    if len(splitted) != 2:
         splitted = text.split("\n", 1)
-        if not len(splitted) == 2:
+        if len(splitted) != 2:
             return ""
-
-    prefix, args = splitted
-    args = args.strip()
-    logging.info(f"Got command {prefix} with args {args}")
-    return args
+    return splitted[1].strip()
 
 
-def display_forwards(forwards: List[Forward]) -> str:
-    """Return a string that beautifully displays all current forwards."""
-    if len(forwards) == 0:
-        return "Currently no forwards are set"
-    forward_str = "This is your configuration"
-    for forward in forwards:
-        forward_str = (
-            forward_str
-            + f"\n\n```\nsource: {forward.source}\ndest: {forward.dest}\n```\n"
-        )
-
-    return forward_str
+def display_forwards(forwards):
+    if not forwards:
+        return "当前没有转发配置"
+    s = "当前配置:"
+    for f in forwards:
+        s += f"\n\n```\nsource: {f.source}\ndest: {f.dest}\n```\n"
+    return s
 
 
-def remove_source(source, forwards: List[Forward]) -> List[Forward]:
-    """Remove a source from forwards."""
-    for i, forward in enumerate(forwards):
-        if forward.source == source:
+def remove_source(source, forwards):
+    for i, f in enumerate(forwards):
+        if f.source == source:
             del forwards[i]
             return forwards
-    raise ValueError("The source does not exist")
+    raise ValueError("源不存在")
 
 
 def get_command_prefix():
     if config.is_bot is None:
-        raise ValueError("config.is_bot is not set!")
-    return "/" if config.is_bot else "\."
+        raise ValueError("config.is_bot 未设置")
+    return "/" if config.is_bot else r"\."
