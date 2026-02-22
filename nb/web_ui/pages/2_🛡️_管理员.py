008ee0ceae021f15afca009216951cf4ea734c09
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-22 12:11:20 +0800

    Update 2_🛡️_管理员.py

--- nb/web_ui/pages/2_🛡️_管理员.py
+++ nb/web_ui/pages/2_🛡️_管理员.py
@@ -7,15 +7,15 @@ from nb.web_ui.utils import get_list, get_string, hide_st, switch_theme
 CONFIG = read_config()
 
 st.set_page_config(
-    page_title="Admins",
+    page_title="管理员",
     page_icon="⭐",
 )
 hide_st(st)
 switch_theme(st,CONFIG)
 if check_password(st):
 
-    CONFIG.admins = get_list(st.text_area("Admins", value=get_string(CONFIG.admins)))
-    st.write("Add the usernames of admins. One in each line.")
+    CONFIG.admins = get_list(st.text_area("管理员列表", value=get_string(CONFIG.admins)))
+    st.write("添加管理员用户名，每行一个。")
 
-    if st.button("Save"):
+    if st.button("保存"):
         write_config(CONFIG)
